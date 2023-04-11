851f5e76829cec540916329a6c078de3cf70d846
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-04-11 12:47:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 // co// Person constructor function
 function Person(name, age) {
+	constructor(name,age){
+		this._name=name;
+		this._age=age;
+	}
+	greet(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`)
+	}
 }
 
 
 // Employee constructor function
 function Employee(name, age, jobTitle) {
+	constructor(name,age,jobTitle){
+	this._jobTitle=jobTitle;
+	super(name,age);
+	}
+	
+	jobGreet(){
+     console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
+	}
+	
 }
 
 // Do not change code below this line
