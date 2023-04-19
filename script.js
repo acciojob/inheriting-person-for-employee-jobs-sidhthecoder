8d7570a555b02f463fb927cfcf118161f82dc51b
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-04-19 21:24:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-// co// Person constructor function
 function Person(name, age) {
-	constructor(name,age){
-		this._name=name;
-		this._age=age;
-	}
-	greet(){
-		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`)
-	}
+this.name = name;
+this.age = age;
 }
 
+Person.prototype.greet = function() {
+console.log(Hello, my name is ${this.name}, I am ${this.age} years old.);
+}
 
-// Employee constructor function
 function Employee(name, age, jobTitle) {
-	constructor(name,age,jobTitle){
-	this._jobTitle=jobTitle;
-	super(name,age);
-	}
-	
-	jobGreet(){
-     console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
-	}
-	
+Person.call(this, name, age);
+this.jobTitle = jobTitle;
 }
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.);
+}
+
+// example usage
+const person1 = new Person('Alice', 25);
+person1.greet(); // Hello, my name is Alice, I am 25 years old.
+
+const employee1 = new Employee('Bob', 30, 'Manager');
+
+employee1.greet(); // Hello, my name is Bob, I am 30 years old.
+employee1.jobGreet(); // Hello, my name is
+
+// // Do not change code below this line
+// window.Person = Person;
+// window.Employee = Employee;
