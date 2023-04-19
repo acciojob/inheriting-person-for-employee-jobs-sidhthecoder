7791446c8dfe3b89a4d30484d216e8f444f2384d
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-04-19 21:29:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,15 +19,6 @@ Employee.prototype.jobGreet = function() {
 console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.);
 }
 
-// example usage
-const person1 = new Person('Alice', 25);
-person1.greet(); // Hello, my name is Alice, I am 25 years old.
-
-const employee1 = new Employee('Bob', 30, 'Manager');
-
-employee1.greet(); // Hello, my name is Bob, I am 30 years old.
-employee1.jobGreet(); // Hello, my name is
-
-// // Do not change code below this line
-// window.Person = Person;
-// window.Employee = Employee;
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
