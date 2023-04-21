64110e3c98fa42bb0c917bfb62a1111f2f551e87
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-04-21 19:16:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ function Person(name, age) {
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 	}
 }
-
-
-
 function Employee(name, age, jobTitle) {
 jobGreet(){
-	console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
  }
 }
-// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
