660266a6b0a65780524b7757f2e8aa29a1641aa8
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-04-21 19:23:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function Person(name, age) {
     greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+		console.log(`Hello,  my name is ${this.name}, I am ${this.age} years old.`);
 	}
 }
+
 function Employee(name, age, jobTitle) extends Person{
-jobGreet(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
- }
+   jobGreet(){
+	  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+   }
 }
+
 window.Person = Person;
 window.Employee = Employee;
