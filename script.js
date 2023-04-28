2302c751d7ad46f236ee9b316fc49362c6527f66
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-04-28 14:20:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+// Person constructor function
 function Person(name, age) {
-    greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	}
+  this.name = name;
+  this.age = age;
 }
 
-function Employee(name, age, jobTitle) extends Person{
-   jobGreet(){
-	  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-   }
+// Add greet() method to Person prototype
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
+
+// Employee constructor function
+function Employee(name, age, jobTitle) {
+  // Call the Person constructor using the Employee instance as the `this` context
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Inherit Person prototype in Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+
+// Set Employee constructor to be itself
+Employee.prototype.constructor = Employee;
+
+// Add jobGreet() method to Employee prototype
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 
 window.Person = Person;
