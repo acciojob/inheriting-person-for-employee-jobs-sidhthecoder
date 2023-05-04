9da0267da41474470845821b1bce320eaacccf01
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-05-04 10:49:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 function Person(name, age) {
-    greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	}
+  this.name = name;
+  this.age = age;
 }
 
-function Employee(name, age, jobTitle) extends Person{
-   jobGreet(){
-	  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-   }
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is Alice and I am 25 years old.`);
+};
+
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
 }
 
+// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
